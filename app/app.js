c7a517006d18b1af5c52fadb0b066cef5df6b6c5
Quethzel <quethzel@gmail.com>
2018-06-02 23:55:01 -0500

    init repo Read & Share

---
+++ app/app.js
@@ -0,0 +1,42 @@
+var App = angular.module('randsApp', [
+    'ngRoute',
+    'ui.router',
+    'ngSanitize'
+]);
+
+App.config(function($stateProvider, $urlRouterProvider, $routeProvider) {
+    $stateProvider
+        // .state('home', {
+        //     url: '/home',
+        //     templateUrl: 'index.html',
+        //     controller: 'homeController'
+        // })
+        .state('about', {
+            url: '/about',
+            templateUrl: 'app/views/about.html',
+            controller: 'aboutCtrl'
+        })
+
+    // $urlRouterProvider.otherwise('/home');
+
+});
+
+// App.run(function($state, $rootScope){
+//    $rootScope.$state = $state;
+// });
+
+// App.controller('homeController', function ($scope, $rootScope, $routeParams, $location, $state) {
+//     // $scope.$on('$locationChangeStart', function(event) {
+//     //     $scope.showMenu = ($location.path() != "/exam") ? true : false;
+//     // });
+  
+// });
+
+App.controller('aboutCtrl', function($scope, $state) {    
+    $scope.version = {
+        system: "1.2.1",
+        database: "4.0.0 firedb",
+        date: "07.06.2017 11:53 p.m."
+    };
+    console.log($scope.version);
+});
